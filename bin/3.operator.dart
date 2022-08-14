4c1ce0d613e8b667c13136d04202c93458a9a8c0
ALI MUJIB <alimujib678@gmail.com>
2022-08-14 21:29:39 +0700

    jenis operator dart

--- bin/3.operator.dart
+++ bin/3.operator.dart
@@ -1,7 +1,39 @@
 void main() {
-  //operator artimatika (*, /, +, -, ++, --)
+  //1.operator artimatika (*, /, +, -, ++, --)
   int kali = 2 * 2;
   print(kali);
-  int bagi = 9 / 3;
+  double bagi = 9 / 3;
   print(bagi);
+  int tambah = 2 + 2;
+  print(tambah);
+  int kurang = 2 - 2;
+  print(kurang);
+  int increment = 2; //ditambah 1 nilai
+  print(++increment); //pre increment -> di tambah dulu baru di tampilkan
+  // print(increment++); //post increment -> di print dulu baru di tampilkan
+  int decrement = 2;
+  print(--decrement); // pre decrement -> di kurang dulu baru di tampilkan
+  // print(decrement--); //post decrement -> di tampilkan dulu baru di kurang
+
+  //2.perbandingan (<, >, ==, <=, >=, !=) -> akan menghasilkan nilai boolean(true/false)
+  bool kurangdari = 10 < 11;
+  print(kurangdari);
+  bool lebihdari = 10 > 15;
+  print(lebihdari);
+  bool samadengan = 10 == 6 + 4;
+  print(samadengan);
+  bool kurangdarismd = 10 <= 11;
+  print(kurangdarismd);
+  bool lebihdarismd = 10 >= 9;
+  print(lebihdarismd);
+  bool tidaksmd = 2 != 2;
+  print(tidaksmd);
+
+  //3.assignment (=, -=, +=, *=, /=)
+  int angka = 2;
+  // angka -= 1; //angka = angka - 1
+  //  angka += 1; //angka = angka + 1
+  //   angka *= 2; //angka = angka * 1
+  angka /= 1; //angka = angka / 1
+  print(angka);
 }
