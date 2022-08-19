1b416fe9b11c44740779e7e6b57e04fed7af9756
ALI MUJIB <alimujib678@gmail.com>
2022-08-19 11:09:16 +0700

    perulangan

---
+++ bin/5.perulangan.dart
@@ -0,0 +1,22 @@
+void main() {
+  ///PERULANGAN
+
+  //1.for
+  // for (int i = 1; i < 50; i++) {
+  //   print('index $i');
+  // }
+
+//2.while
+  // int i = 1;
+  // while (i < 50) {
+  //   print(i);
+  //   i++;
+  // }
+
+  //3.do while
+  int i = 1;
+  do {
+    print(i);
+    i++;
+  } while (i <= 50);
+}
